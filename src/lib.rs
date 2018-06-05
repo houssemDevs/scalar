dbd0a8aff6e821501f5457c571f31556728c2112
Houssem GHIAT <ghiat.houssem@gmail.com>
2018-06-05 13:52:53 +0100

    cleaned up the trait from weired things

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,49 @@
 #![feature(op_assign_traits)]
-#![feature(zero_one)]
 
-use std::num::*;
 use std::default::*;
 use std::ops::*;
 
 mod abs;
-mod trigm;
-mod math_func;
 
 pub use abs::*;
-pub use trigm::*;
-pub use math_func::*;
 
 pub trait Scalar:
-    Copy + PartialEq + PartialOrd
-    +One + Zero
-    +Default
-    +Add<Self, Output=Self> + AddAssign<Self>
-    +Sub<Self, Output=Self> + SubAssign<Self>
-    +Mul<Self, Output=Self> + MulAssign<Self>
-    +Div<Self, Output=Self> + DivAssign<Self>
-    +Rem<Self, Output=Self> + RemAssign<Self>
-    +Neg<Output=Self>
-    +Abs
-    +Trigonometry
-    +MathFunc
-    {}
+    Copy
+    + PartialEq
+    + PartialOrd
+    + Default
+    + Add<Self, Output = Self>
+    + AddAssign<Self>
+    + Sub<Self, Output = Self>
+    + SubAssign<Self>
+    + Mul<Self, Output = Self>
+    + MulAssign<Self>
+    + Div<Self, Output = Self>
+    + DivAssign<Self>
+    + Rem<Self, Output = Self>
+    + RemAssign<Self>
+    + Neg<Output = Self>
+    + Abs
+{
+}
 
 impl<S> Scalar for S
-    where S:
-    Copy + PartialEq + PartialOrd
-    +One + Zero
-    +Default
-    +Add<S, Output=S> + AddAssign<S>
-    +Sub<S, Output=S> + SubAssign<S>
-    +Mul<S, Output=S> + MulAssign<S>
-    +Div<S, Output=S> + DivAssign<S>
-    +Rem<S, Output=S> + RemAssign<S>
-    +Neg<Output=S>
-    +Abs
-    +Trigonometry
-    +MathFunc
-    {}
-
+where
+    S: Copy
+        + PartialEq
+        + PartialOrd
+        + Default
+        + Add<S, Output = S>
+        + AddAssign<S>
+        + Sub<S, Output = S>
+        + SubAssign<S>
+        + Mul<S, Output = S>
+        + MulAssign<S>
+        + Div<S, Output = S>
+        + DivAssign<S>
+        + Rem<S, Output = S>
+        + RemAssign<S>
+        + Neg<Output = S>
+        + Abs,
+{
+}
